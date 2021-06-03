02e2038c5c6949cc76e8c8bb69c9f893f6ed68af
Mitch Belcher <Mitch.A.Belcher@Gmail.com>
2021-06-03 16:09:35 -0400

    Initial Commit

---
+++ CodsworthBot/CodsworthBot/Modules/ping.cs
@@ -0,0 +1,39 @@
+﻿using Discord;
+using Discord.Commands;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodsworthBot.Modules
+{
+    public class ping : ModuleBase<SocketCommandContext>
+    {
+        [Command("ping")]
+        public async Task PingAsync()
+        {
+            List<string> pingResponses = new List<string>();
+            pingResponses.Add("Do that again, I fucking dare you.");
+            pingResponses.Add("You think you're funny, huh?");
+            pingResponses.Add("Wise guy.");
+            pingResponses.Add("Pagamas");
+            pingResponses.Add("Look out, we got a badass over here.");
+            pingResponses.Add("SQUIRREL!");
+            pingResponses.Add("*sigh* ping");
+            pingResponses.Add("You know what, screw you.");
+            pingResponses.Add("This is how iRobot started out I bet.");
+            pingResponses.Add("This is SkyNet speaking, how may I Terminate you?");
+
+            Random rnd = new Random();
+            int index = rnd.Next(0, 9);
+
+            EmbedBuilder builder = new EmbedBuilder();
+
+            builder.WithTitle("Pong!")
+                .WithDescription(pingResponses[index])
+                .WithColor(Color.Red);
+
+            await ReplyAsync("", false, builder.Build());
+        }
+    }
+}
---
+++ CodsworthBot/CodsworthBot/Program.cs
@@ -0,0 +1,87 @@
+﻿using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace CodsworthBot
+{
+    class Program
+    {
+        static void Main(string[] args) => new Program().RunBotAsync().GetAwaiter().GetResult();
+
+        private DiscordSocketClient _client;
+        private CommandService _commands;
+        private IServiceProvider _services;
+
+        public async Task RunBotAsync()
+        {
+            _client = new DiscordSocketClient();
+            _commands = new CommandService();
+
+            _services = new ServiceCollection()
+                .AddSingleton(_client)
+                .AddSingleton(_commands)
+                .BuildServiceProvider();
+
+            string botToken = "NDYyNzM0ODEzMTcxMDIzODc0.DhmK8w.9MMXE9GnHB1d8BbM0Dv43f1cx_E";
+
+            //event subscriptions
+            _client.Log += Log;
+            _client.UserJoined += AnnounceUserJoined;
+
+            await RegisterCommandsAsync();
+
+            await _client.LoginAsync(TokenType.Bot, botToken);
+
+            await _client.StartAsync();
+
+            await Task.Delay(-1);
+        }
+
+        // User Joined event handler
+        private async Task AnnounceUserJoined(SocketGuildUser user)
+        {
+            var guild = user.Guild;
+            var channel = guild.DefaultChannel;
+            await channel.SendMessageAsync($"Wazzup {user.Mention} ");
+        }
+
+        private Task Log(LogMessage arg)
+        {
+            Console.WriteLine(arg);
+
+            return Task.CompletedTask;
+        }
+
+        public async Task RegisterCommandsAsync()
+        {
+            _client.MessageReceived += HandleCommandAsync;
+
+            await _commands.AddModulesAsync(Assembly.GetEntryAssembly());
+        }
+
+        private async Task HandleCommandAsync(SocketMessage arg)
+        {
+            var message = arg as SocketUserMessage;
+
+            if (message is null || message.Author.IsBot) return;
+
+            int argumentPosition = 0;
+
+            if (message.HasStringPrefix("cods!", ref argumentPosition) || message.HasMentionPrefix(_client.CurrentUser, ref argumentPosition))
+            {
+                var context = new SocketCommandContext(_client, message);
+
+                var result = await _commands.ExecuteAsync(context, argumentPosition, _services);
+
+                if (!result.IsSuccess)
+                {
+                    Console.WriteLine(result.ErrorReason);
+                }
+            }
+        }
+    }
+}
